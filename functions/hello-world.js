34688c6059f971164fb9acb7777122c90b90783a
Anupam Singh <53266699+anupamsinghdoon@users.noreply.github.com>
2020-10-13 23:41:23 +0530

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
     const result = {
-        message : "Function modified by visual studio ui edit true"
+        message : "Function modified by visual studio ui editable true"
     }
 
     callback(null, result);
