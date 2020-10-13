e282f9e00aa5db0003b529885eb1a7b914dcf3e7
anupamsinghdoon <singh.anupam.doon@gmail.com>
2020-10-14 00:22:35 +0530

    Added by Anupam Branch

---
+++ functions/Anupam.js
@@ -0,0 +1,7 @@
+exports.handler = function (context, event, callback) {
+    const result = {
+        message: "Function modified by visual studio. By Anupam"
+    }
+
+    callback(null, result);
+};
