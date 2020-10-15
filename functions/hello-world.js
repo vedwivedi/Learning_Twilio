8323e5f8d5f14e634e74054237ec457df13d5299
Anupam Singh <53266699+anupamsinghdoon@users.noreply.github.com>
2020-10-16 01:07:18 +0530

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
     const result = {
-        message : "Function modified by visual studio."
+        message : "Function modified by visual studio. Commit manually"
     }
 
     callback(null, result);
