729b022c529e553b24feae48f1c8dc3e5f168b97
Anupam Singh <53266699+anupamsinghdoon@users.noreply.github.com>
2020-10-16 01:42:39 +0530

    Update Veerendra.js

--- functions/Veerendra.js
+++ functions/Veerendra.js
@@ -1,6 +1,6 @@
 exports.handler = function (context, event, callback) {
     const result = {
-        message: "Function modified by visual studio. By Veerendra"
+        message: "Function modified by visual studio. By Veerendra by manually"
     }
 
     callback(null, result);
