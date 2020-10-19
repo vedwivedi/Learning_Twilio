0b551abf5c35eb88b865bba421376a7d7b657c03
Veerendra Dwivedi <71781046+vedwivedi@users.noreply.github.com>
2020-10-19 13:08:11 -0700

    Delete hello-world.js

--- functions/hello-world.js
+++
@@ -1,7 +0,0 @@
-exports.handler = function(context, event, callback) {
-    const result = {
-        message : "Function modified by visual studio. Commit manually"
-    }
-
-    callback(null, result);
-  };
