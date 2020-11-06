6d73209c4437f5cb347f48cfdf42066c31ebbb86
Veerendra Dwivedi <71781046+vedwivedi@users.noreply.github.com>
2020-11-07 00:49:34 +0530

    Delete Veerendra.js

--- functions/Veerendra.js
+++
@@ -1,7 +0,0 @@
-exports.handler = function (context, event, callback) {
-    const result = {
-        message: "Function modified by visual studio. By Veerendra by manually"
-    }
-
-    callback(null, result);
-};
